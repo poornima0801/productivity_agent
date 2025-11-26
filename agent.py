80d1c4b1ac1f1197f972eff4f40bd4406a0c402d
Sarath Sabu <sarathbrn99@gmail.com>
2025-11-26 18:19:24 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -7,7 +7,11 @@ load_dotenv(dotenv_path="/workspaces/productivity_agent/.env")
 from openai import OpenAI
 
 # Initialize once with API key
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    project=os.getenv("OPENAI_PROJECT_ID")
+)
+
 
 import json
 
